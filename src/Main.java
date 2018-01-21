b6701bd2ef45e00f4649ce49bb4384f3ea7692ea
Derek Nowicki <dereknowicki@gmail.com>
2018-01-21 05:16:01 -0800

    add user command menu

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,119 @@
  * assignment: program 1
  * date last modified: 2018-01-21
  * 
- * purpose: This program accepts integer input from the command line, doubles the
- * value of input; the result is output to both the screen and a file called data.txt
+ * purpose: This program accepts from the standard input a sequence
+ * of integer values with each value separated by a space.
+ * It builds a binary search tree using these values in the order they are entered
+ * then prints 3 traversals: pre-order, in-order, and post-order.
  * 
  ****************************************************************/
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+	private static String [] command;
+	private static String numbers;
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private static String [] arrOfStr;
+
+	static void printCommandMenu() {
+		System.out.println("Please Enter a Command");
+		System.out.println("I: Insert a value");
+		System.out.println("D: Delete a value");
+		System.out.println("P: Find predecessor");
+		System.out.println("S: Find successor");
+		System.out.println("E: Exit the program");
+		System.out.println("H: Display this message");
+	}
+	
+	static void getNumbers() {
+		System.out.println("Please enter the initial sequence of values:");
+		try {
+			numbers = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(numbers);
+		
+		arrOfStr = numbers.split(" ");
+		
+		for (String a : arrOfStr)
+			System.out.println(a);
+	}
+	
+	static void getCommand() {
+		String cmdString = "";
+		try {
+			cmdString = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		command = cmdString.split(" ");
+	}
+	
+	static void handleInsertCommand() {
+		if(command.length > 1)
+			System.out.println("I: Insert a value -> " + command[1]);
+		else
+			System.out.println("Too few arguments");
+	}
+	
+	static void handleDeleteCommand() {
+		if(command.length > 1)
+			System.out.println("D: Delete a value -> " + command[1]);
+		else
+			System.out.println("Too few arguments");
+	}
+	
+	static void handlePredecessorCommand() {
+		if(command.length > 1)
+			System.out.println("P: Find predecessor -> " + command[1]);
+		else
+			System.out.println("Too few arguments");
+	}
+	
+	static void handleSuccessorCommand() {
+		if(command.length > 1)
+			System.out.println("S: Find successor -> " + command[1]);
+		else
+			System.out.println("Too few arguments");
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		getNumbers();
+		
+		while(true) {
+			printCommandMenu();
+			getCommand();
+			
+			switch(Character.toUpperCase(command[0].charAt(0))) {
+			case 'I':
+				handleInsertCommand();
+				break;
+			case 'D':
+				handleDeleteCommand();
+				break;
+			case 'P':
+				handlePredecessorCommand();
+				break;
+			case 'S':
+				handleSuccessorCommand();
+				break;
+			case 'E':
+				System.exit(0);
+				break;
+			case 'H':
+				printCommandMenu();
+				break;
+			default:
+				System.out.println("Invalid command, try again -> " + command[0]);
+				break;
+			}
+			
+		}
 
 	}
-
 }
