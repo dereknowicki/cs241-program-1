d81d1ff1340fd2951bd4b42a56e4484200687678
Derek Nowicki <dereknowicki@gmail.com>
2018-01-21 04:11:52 -0800

    add required comment block

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+/****************************************************************
+ * file: Main.java 
+ * author: Derek Nowicki
+ * class: CS 141 – Programming and Problem Solving
+ * 
+ * assignment: program 1
+ * date last modified: 2018-01-21
+ * 
+ * purpose: This program accepts integer input from the command line, doubles the
+ * value of input; the result is output to both the screen and a file called data.txt
+ * 
+ ****************************************************************/
 
 public class Main {
 
